f2ecc23f975857de57b7d0c83d55a0af1fe56621
Ingrid Silva <ingrid.arnaud.ea@gmail.com>
2025-11-26 13:43:24 -0300

    Menu Update

---
+++ script.js
@@ -0,0 +1,18 @@
+const icon = document.querySelector(".img-mobile");
+const menu = document.querySelector(".header-menu");
+
+icon.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+
+// --------------------zoom salas-----------------------
+
+function abrirZoom(img) {
+    document.getElementById("imagemZoom").src = img.src;
+    document.getElementById("zoom").style.display = "flex";
+}
+
+function fecharZoom() {
+    document.getElementById("zoom").style.display = "none";
+}
